dc4d328e784c2c3e6be04bcdee603d8dce5eb6c2
MiskirB <miskerbab@gmail.com>
2025-05-30 16:21:59 +0300

    Added notebook 01_eda_headlines.ipynb for

---
+++ scripts/headline_stats.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def headline_length_analysis(file_path):
+    df = pd.read_csv(file_path)
+    df['headline_length'] = df['headline'].apply(len)
+    print(df['headline_length'].describe())
+
+    plt.figure(figsize=(10, 5))
+    sns.histplot(df['headline_length'], bins=30, kde=True)
+    plt.title("Headline Length Distribution")
+    plt.xlabel("Characters")
+    plt.ylabel("Frequency")
+    plt.show()
+
+if __name__ == "__main__":
+    headline_length_analysis('../data/TSLA_historical_data.csv')
+
